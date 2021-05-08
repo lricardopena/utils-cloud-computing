6413d065018185590926e566113085ad68729aae
Luis Pena <luis.pena@trackstreet.com>
2021-05-07 20:54:09 -0500

    Add the file storage for the S3

---
+++ file_storage/storage_service/s3.py
@@ -0,0 +1,91 @@
+import os
+from datetime import datetime
+from io import BytesIO
+
+import boto3
+
+from file_storage.file_storage_manager import FileStorageManager
+
+
+class S3(FileStorageManager):
+    def __init__(self, bucket: str, region_name: str):
+        """
+        :param region_name: The region to connect to AWS
+        """
+        self.client = boto3.client('s3', region_name=region_name)
+        self.paginator = self.client.get_paginator("list_objects_v2")
+        self.bucket = bucket
+        super().__init__()
+
+    def put_file(self, file_name: str, value: BytesIO, bucket: str):
+        """
+        This function put some object in the bucket named (key) as send by the file_name
+        :param file_name: Name to save in the bucket
+        :param value: The buffer to save in the
+        :param bucket: Bucket name to save the file
+        :return: None
+        """
+        value.seek(0)
+        self.client.upload_fileobj(value, bucket, file_name)
+
+    def get_file(self, file_name: str, bucket: str) -> BytesIO:
+        """
+        Function that gets the file from the file_name
+        :param file_name:
+        :return:
+        """
+        fl = BytesIO()
+        self.client.download_fileobj(self.bucket, os.path.join(self.base_path, file_name), fl)
+        fl.seek(0)
+        return fl
+
+    def get_last_update(self, file_name: str) -> datetime:
+        pages = self.paginator.paginate(
+            Bucket=self.bucket,
+            Prefix=os.path.join(self.base_path, file_name)
+        )
+
+        for page in pages:
+            for paged_file in page['Contents']:
+                return paged_file['LastModified']
+        # If the file isn't there we return an long date
+        return datetime(1, 1, 1, 0, 0)
+
+    def get_filenames_prefix(self, prefix):
+        pages = self.paginator.paginate(
+            Bucket=self.bucket,
+            Prefix=self.base_path + prefix
+        )
+        file_names = []
+
+        for page in pages:
+            for paged_file in page['Contents']:
+                file_names.append(paged_file['Key'])
+                print(paged_file['Key'])
+
+        return [filename[len(self.base_path):] for filename in file_names]
+
+    def file_exists(self, file_name: str) -> bool:
+        pages = self.paginator.paginate(
+            Bucket=self.bucket,
+            Prefix=os.path.join(self.base_path, file_name)
+        )
+
+        for page in pages:
+            for paged_file in page['Contents']:
+                return True
+        # If the file isn't there
+        return False
+
+    def move_filenames_to_prefix(self, file_names: list, prefix: str):
+        for file_name in file_names:
+            copy_source = {
+                'Bucket': self.bucket,
+                'Key': os.path.join(self.base_path, file_name)
+            }
+
+            self.client.copy(copy_source, self.bucket, os.path.join(prefix, file_name))
+            self.delete(file_name)
+
+    def delete(self, file_name: str):
+        self.client.delete_object(Bucket=self.bucket, Key=os.path.join(self.base_path, file_name))
