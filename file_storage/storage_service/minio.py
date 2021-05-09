d4d3893cfb80445a8424e4a3fc6a7e2dd17f39d9
Luis Pena <luis.pena@trackstreet.com>
2021-05-08 21:25:35 -0500

    Change how we connect with MinIO for more compact use (is almost the same that localstack S3)

--- file_storage/storage_service/minio.py
+++ file_storage/storage_service/minio.py
@@ -1,17 +1,13 @@
-import boto3
-
 from botocore.config import Config
 
 from file_storage.storage_service.s3 import S3
 
 
 class Minio(S3):
-    def __init__(self, region_name: str, endpoint_minio: str):
+    def __init__(self, bucket: str, region_name: str, endpoint_minio: str):
         """
+        :param bucket: Bucket Name to save the files in MinIO
         :param region_name: Name of the region to connect with the API
+        :param endpoint_minio: The endpoint to connect with MinIO
         """
-        super().__init__(region_name)
-        self.s3 = boto3.client('s3', endpoint_url=endpoint_minio,
-                               config=Config(signature_version='s3v4'),
-                               region_name=region_name)
-        self.paginator = self.s3.get_paginator("list_objects_v2")
+        super().__init__(bucket, region_name, endpoint_url=endpoint_minio, config=Config(signature_version='s3v4'))
