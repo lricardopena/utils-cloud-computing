118c2eef22552eb3e59c76235578d5d9b687674c
Luis Pena <luis.pena@trackstreet.com>
2021-05-08 22:25:38 -0500

    Add the function to check if a file exist and add a TODO for implement the function that moves files to other path

--- file_storage/storage_service/localstorage.py
+++ file_storage/storage_service/localstorage.py
@@ -31,12 +31,22 @@ class LocalStorage(FileStorageManager):
             os.remove(final_path)
 
     def get_last_update(self, file_name: str) -> datetime:
-        final_path = self.final_path(file_name)
-        if not os.path.isfile(final_path):
+        if not self.file_exists(file_name):
             return datetime(1, 1, 1, 0, 0)
+
+        final_path = self.final_path(file_name)
         modified_time_unix = os.path.getmtime(final_path)
         modification_time = datetime.fromtimestamp(modified_time_unix)
         return modification_time
+
+    def file_exists(self, file_name: str) -> bool:
+        final_path = self.final_path(file_name)
+        return os.path.isfile(final_path)
+
+    def move_filenames_to_prefix(self, file_names: list, prefix: str):
+        # Not implemented yet
+        # TODO Implement this function
+        pass
 
     def get_filenames_prefix(self, prefix: str):
         name_files = [pos_json for pos_json in os.listdir(self.base_path + prefix)]
