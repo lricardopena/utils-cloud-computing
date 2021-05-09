0c1a094e121c14eed464fb16fb7dc3f5f428033f
Luis Pena <luis.pena@trackstreet.com>
2021-05-08 21:22:19 -0500

    Change how we initialize the class if the localstack is used for storage service

--- file_storage/storage_service/localstack_s3.py
+++ file_storage/storage_service/localstack_s3.py
@@ -1,4 +1,3 @@
-import boto3
 from botocore.config import Config
 
 from file_storage.storage_service.s3 import S3
@@ -11,8 +10,6 @@ class LocalStackS3(S3):
         :param region_name: Name of the region to connect with the API
         :param endpoint_localstack: Is the endpoint where the localstack is running, usually
         """
-        self.bucket = bucket
-        self.client = boto3.client('s3', endpoint_url=endpoint_localstack,
-                                   config=Config(signature_version='s3v4'),
-                                   region_name=region_name)
-        self.paginator = self.client.get_paginator("list_objects_v2")
+
+        super().__init__(bucket, region_name, endpoint_url=endpoint_localstack,
+                         config=Config(signature_version='s3v4'))
